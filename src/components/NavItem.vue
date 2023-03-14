ed5f4a3b21711a50cfbf5af6c7d5e702d0f93d6e
Edmond Chuc <edmond@kurrawong.ai>
2023-03-14 11:29:34 +1000

    Update nav item colours

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -2,7 +2,7 @@
   <li class="list-none">
     <a
       :href="href"
-      class="py-4 px-4 text-[#980000] font-medium hover:underline hover:bg-[#9800002d] hover:cursor-pointer"
+      class="py-4 px-4 font-medium hover:underline hover:bg-[#3366ff23] hover:cursor-pointer"
       >{{ label }}</a
     >
   </li>
