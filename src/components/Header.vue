378687d91f6e5981588a59324a4e6c7290cbb671
Edmond Chuc <edmond@kurrawong.ai>
2023-03-25 14:41:48 +1000

    WIP - cleaner header and landing page design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,24 @@
 <template>
-  <div
-    class="bg-[#ffffff] border-solid border-b-2 border-[#ffffff] drop-shadow-md"
+  <header
+    class="bg-white border-b border-gray-200 fixed h-16 inset-x-0 min-[940px]:border-0 min-[940px]:h-auto min-[940px]:relative top-0 z-10"
   >
-    <div class="container mx-auto min-w-[320px]">
-      <header class="flex justify-between items-center px-2">
-        <div class="flex items-center min-w-[175px]">
+    <div class="mx-auto max-w-7xl h-full px-4 sm:px-6">
+      <div
+        class="flex items-center justify-between h-full min-[940px]:py-6 xl:justify-start xl:space-x-10"
+      >
+        <div class="flex justify-start min-[940px]:flex-1 min-[940px]:w-0">
           <a href="/">
             <img
               src="/KurrawongAI_350.png"
               alt="Kurrawong AI logo"
-              width="350"
-              height="143"
+              width="130"
             />
           </a>
         </div>
-        <nav class="pr-3 overflow-x-auto py-4">
+
+        <nav class="hidden min-[940px]:flex items-center space-x-8">
           <ul class="flex items-center m-0">
+            <DropdownMenu></DropdownMenu>
             <NavItem
               v-for="item in items"
               :label="item.label"
@@ -23,13 +26,14 @@
             />
           </ul>
         </nav>
-      </header>
+      </div>
     </div>
-  </div>
+  </header>
 </template>
 
 <script setup lang="ts">
 import NavItem from "./NavItem.vue";
+import DropdownMenu from "./DropdownMenu.vue";
 
 defineProps<{ items: { label: string; href: string }[] }>();
 </script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+      },
+    },
+  },
+  plugins: [],
+};
